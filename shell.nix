b0025f2acd395bf0f7cd6d39795af207eb582b79
Malo Bourgon <mbourgon@gmail.com>
2020-10-08 12:14:28 -0700

    Update nix-shell to use haskell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
-{ pkgs ? import (import ./nix/sources.nix).nixpkgs {} }:
-
-with pkgs;
-mkShell {
+let
+  pkgs = import (import ./nix/sources.nix).nixpkgs {};
+  hsPkgs = import ./default.nix {};
+  compiler-name = hsPkgs.prefmanager.project.pkg-set.config.compiler.nix-name;
+in hsPkgs.shellFor {
   buildInputs = [
-    haskell.packages.ghc884.haskell-language-server
-    cabal2nix
-    stack
-    niv
+    pkgs.haskell.packages.${compiler-name}.haskell-language-server
+    pkgs.stack
+    pkgs.niv
   ];
 }
