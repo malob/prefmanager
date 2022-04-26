d9aa23a9266a2858a46553379417435be29d28e2
Malo Bourgon <mbourgon@gmail.com>
2022-04-26 12:43:14 -0700

    Update to GHC 9.0.2 and other cleanup

--- flake.nix
+++ flake.nix
@@ -7,24 +7,22 @@
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
     flake-utils.url = "github:numtide/flake-utils";
     plist-source = { url = "github:malob/plist/monadfail"; flake = false; };
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devshell.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs = { self, devshell, nixpkgs, flake-utils, plist-source, ... }:
     flake-utils.lib.eachSystem [ "x86_64-darwin" "aarch64-darwin" ] (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      compiler =  pkgs.haskell.packages.ghc8107;
+      compiler =  pkgs.haskell.packages.ghc902;
       hlib = pkgs.haskell.lib;
       plist = hlib.markUnbroken (hlib.overrideSrc compiler.plist { src = plist-source; });
       prefmanager = compiler.callCabal2nix "prefmanager" ./. { inherit plist; };
       mkShell = devshell.legacyPackages.${system}.mkShell;
     in rec {
       # Built by `nix build .`
-      defaultPackage = prefmanager;
-      packages.prefmanager = defaultPackage;
-
-      # Run `prefmanager` with `nix run .`
-      defaultApp = { type = "app"; program = "${prefmanager}/bin/prefmanager"; };
-      apps.prefmanager = defaultApp;
+      packages.default = prefmanager;
+      packages.prefmanager = prefmanager;
 
       # # This is used by `nix develop .`
       devShell = mkShell {
@@ -33,7 +31,6 @@
           compiler.haskell-language-server
           compiler.implicit-hie
           compiler.weeder
-          pkgs.cabal2nix
           pkgs.stack
           pkgs.hlint
         ];
@@ -43,12 +40,6 @@
             name = "hie";
             category = "project";
             command = "gen-hie > hie.yaml";
-          }
-          {
-            help = "Update prefmanager.nix (run from project root)";
-            name = "2nix";
-            category = "project";
-            command = "cabal2nix --hpack . > prefmanager.nix";
           }
         ];
       };
--- stack.nix
+++ stack.nix
@@ -1,5 +1,5 @@
 with import (import ./default.nix).inputs.nixpkgs {};
 haskell.lib.buildStackProject {
-  ghc = haskell.compiler.ghc8107;
+  ghc = haskell.compiler.ghc902;
   name = "prefmanager";
 }
