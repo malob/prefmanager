294646ea80d7df80c3c3cf5207602d4b7a6236b8
Malo Bourgon <mbourgon@gmail.com>
2023-05-30 10:23:20 -0700

    Update to GHC 9.2.7

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
     flake-utils.url = "github:numtide/flake-utils";
     plist-source = { url = "github:malob/plist/monadfail"; flake = false; };
     devshell.inputs.nixpkgs.follows = "nixpkgs";
-    devshell.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs = { self, devshell, nixpkgs, flake-utils, plist-source, ... }: {
@@ -18,7 +17,7 @@
   } // flake-utils.lib.eachSystem [ "x86_64-darwin" "aarch64-darwin" ] (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-      compiler = pkgs.haskell.packages.ghc926;
+      compiler = pkgs.haskell.packages.ghc927;
       hlib = pkgs.haskell.lib;
       plist = hlib.markUnbroken (hlib.overrideSrc compiler.plist { src = plist-source; });
       prefmanager = compiler.callCabal2nix "prefmanager" ./. { inherit plist; };
--- stack.nix
+++ stack.nix
@@ -1,5 +1,5 @@
 with import (import ./default.nix).inputs.nixpkgs {};
 haskell.lib.buildStackProject {
-  ghc = haskell.compiler.ghc926;
+  ghc = haskell.compiler.ghc927;
   name = "prefmanager";
 }
