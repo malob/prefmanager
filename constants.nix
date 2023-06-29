939cec996dfee707bd554d00f92f75a071703872
Malo Bourgon <mbourgon@gmail.com>
2023-06-28 17:11:58 -0700

    Disable GHC 9.4.5 and 9.6.2 due to denpendency build failure

--- constants.nix
+++ constants.nix
@@ -1,5 +1,6 @@
 {
   name = "prefmanager";
-  ghcVersions = map (v: "ghc${v}") [ "8107" "902" "928" "945" "962" ];
+  # GHC 9.4.5 and 9.6.2 are disabled since `fgl` a dependency of `async-pool` fails to build.
+  ghcVersions = map (v: "ghc${v}") [ "8107" "902" "928" /* "945" "962" */ ];
   defaultGhcVersion = "ghc928";
 }
