4907b56b7d2392aa942f6523ee4e7df13bb6e9bb
Malo Bourgon <mbourgon@gmail.com>
2023-06-28 18:42:07 -0700

    Remove periods

--- app/Main.hs
+++ app/Main.hs
@@ -26,32 +26,32 @@ commands = hsubparser
          (   watch . fromList <$> some
                (DomainName <$> strArgument
                  (  metavar "DOMAIN..."
-                 <> help "Domain(s) that will be watched."
+                 <> help "Domain(s) that will be watched"
                  <> completer domainCompleter
                  )
                )
          <|> flag' (watch =<< domains)
                (  long  "all"
                <> short 'a'
-               <> help  "Watch all domains including NSGlobalDomain."
+               <> help  "Watch all domains including NSGlobalDomain"
                )
          )
-         $ progDesc "Watch domain(s) for changes."
+         $ progDesc "Watch domain(s) for changes"
        )
   <> command "domains"
        (info
          (pure printDomains)
-         (progDesc "List all domains.")
+         (progDesc "List all domains")
        )
   <> command "keys"
        (info
          (printKeys . DomainName <$> strArgument
            (  metavar "DOMAIN"
-           <> help "A domain for which to list keys."
+           <> help "A domain for which to list keys"
            <> completer domainCompleter
            )
          )
-         $ progDesc "List the current keys in a domain."
+         $ progDesc "List the current keys in a domain"
        )
   )
     where
