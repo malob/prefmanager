48bb1ad4e660a7840c7ffcad4ba2c0ff6727b208
Malo Bourgon <mbourgon@gmail.com>
2024-03-25 12:11:37 -0700

    Update to GHC 9.6.4

--- constants.nix
+++ constants.nix
@@ -1,6 +1,5 @@
 {
   name = "prefmanager";
-  # GHC 9.6.2 disabled since `fgl` a dependency of `async-pool` fails to build.
   ghcVersions = map (v: "ghc${v}") [ "810" "90" "92" "94" "96" ];
-  defaultGhcVersion = "ghc94";
+  defaultGhcVersion = "ghc96";
 }
